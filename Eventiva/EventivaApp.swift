3a4b5d3661e788dc8ef8e27a879eb6d24c38eb0a
Shakthi Sachintha <43114582+shakthisachintha@users.noreply.github.com>
2023-08-14 22:28:26 +0530

    Initial Commit

---
+++ Eventiva/EventivaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventivaApp.swift
+//  Eventiva
+//
+//  Created by Shakthi Sachintha on 2023-08-14.
+//
+
+import SwiftUI
+
+@main
+struct EventivaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
