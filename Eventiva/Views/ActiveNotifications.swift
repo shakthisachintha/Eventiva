7a6d6b3becc3eb9f7a0e5a043973581fc81466fb
Shakthi Sachintha <43114582+shakthisachintha@users.noreply.github.com>
2023-08-21 23:30:20 +0530

    latest changes

---
+++ Eventiva/Views/ActiveNotifications.swift
@@ -0,0 +1,20 @@
+//
+//  ActiveNotifications.swift
+//  Eventiva
+//
+//  Created by Shakthi Sachintha on 2023-08-21.
+//
+
+import SwiftUI
+
+struct ActiveNotifications: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActiveNotifications_Previews: PreviewProvider {
+    static var previews: some View {
+        ActiveNotifications()
+    }
+}
---
+++ Eventiva/Views/AppInfo.swift
@@ -0,0 +1,20 @@
+//
+//  AppInfo.swift
+//  Eventiva
+//
+//  Created by Shakthi Sachintha on 2023-08-21.
+//
+
+import SwiftUI
+
+struct AppInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppInfo_Previews: PreviewProvider {
+    static var previews: some View {
+        AppInfo()
+    }
+}
